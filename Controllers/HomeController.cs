e9e5d667ec997fcf501f5f55b9083a903563563a
rjainchambal <rjain@chambal.com>
2015-03-04 16:27:21 +0530

    different insert operations
    insertBatch Array and insert using BulkWriteOperation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Configuration;
 using System.Web.Mvc;
 using MongoDbSample.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
 
@@ -8,23 +9,25 @@ namespace MongoDbSample.Controllers
 {
     public class HomeController : Controller
     {
-        public MongoDatabase db;
+        public MongoDatabase Db;
 
-        string uri = WebConfigurationManager.ConnectionStrings["MongoDbUri"].ConnectionString;
+        readonly string _uri = WebConfigurationManager.ConnectionStrings["MongoDbUri"].ConnectionString;
 
         public HomeController()
         {
             //const string uri = "mongodb://admin:admin@ds031277.mongolab.com:31277/sampledb";
-            MongoUrl url = new MongoUrl(uri);
+            MongoUrl url = new MongoUrl(_uri);
             MongoClient client = new MongoClient(url);
             MongoServer server = client.GetServer();
-            db  = server.GetDatabase("sampledb");
+            Db = server.GetDatabase("sampledb");
         }
 
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
             MongoDbOpeartions();
+            InsertBatch();
+            InsertUsingBulk();
             return View();
         }
 
@@ -40,11 +43,11 @@ namespace MongoDbSample.Controllers
             return View();
         }
 
-        public  void MongoDbOpeartions()
+        public void MongoDbOpeartions()
         {
 
 
-            var collectionCustomer = db.GetCollection<Customer>("customer");
+            var collectionCustomer = Db.GetCollection<Customer>("customer");
 
             //BsonDocument[] seedData = CreateSeedData();
             //customer.InsertBatch(seedData);
@@ -52,8 +55,8 @@ namespace MongoDbSample.Controllers
             //insert
             Customer customer1 = new Customer { Name = "Apurva Jain", Address = "Udaipur", Country = "India", Phone = "999999999" };
             collectionCustomer.Insert(customer1);
-            
-            Customer customer2 = new Customer { Name = "Ronak Jain",Address = "Rishabhdeo", Country = "India", Phone = "9887594812"};
+
+            Customer customer2 = new Customer { Name = "Ronak Jain", Address = "Rishabhdeo", Country = "India", Phone = "9887594812" };
             collectionCustomer.Insert(customer2);
             var id = customer2.Id;
 
@@ -61,7 +64,7 @@ namespace MongoDbSample.Controllers
             //find
             var query = Query<Customer>.EQ(e => e.Id, id);
             Customer cust = collectionCustomer.FindOne(query);
-   
+
             //save
             cust.Address = "Rishabhdeo, Udaipur, Rajasthan";
             collectionCustomer.Save(cust);
@@ -74,9 +77,79 @@ namespace MongoDbSample.Controllers
             collectionCustomer.Remove(query);
         }
 
-        public ActionResult Adddata(string datatoinsert)
+        public void InsertBatch()
         {
-            return Json();
+            var collectionCustomer = Db.GetCollection("customer");
+            BsonDocument subBson1 = new BsonDocument
+            {
+                {"model", "14Q3"},
+                {"manufacturer", "XYZ Company"}
+            };
+
+            BsonDocument subBson2 = new BsonDocument
+            {
+                {"size", "S"},
+                {"qty", "25"}
+            };
+
+            BsonDocument subBson3 = new BsonDocument
+            {
+                {"size", "M"},
+                {"qty", "50"}
+            };
+
+            var documents = new BsonArray { subBson2, subBson3 };
+
+            BsonDocument bson = new BsonDocument
+            {
+                {"item", "ABC1"},
+                {"details", subBson1},
+                {"stock", documents},
+                {"category", "clothing"}
+            };
+
+            BsonDocument[] seedData = { bson, subBson1, subBson2, subBson3 };
+            collectionCustomer.InsertBatch(seedData);
+            //collectionCustomer.InitializeOrderedBulkOperation()
+        }
+
+        public void InsertUsingBulk()
+        {
+            var collectionCustomer = Db.GetCollection("customer");
+            BsonDocument subBson1 = new BsonDocument
+            {
+                {"model", "14Q3"},
+                {"manufacturer", "XYZ Company"}
+            };
+
+            BsonDocument subBson2 = new BsonDocument
+            {
+                {"size", "S"},
+                {"qty", "25"}
+            };
+
+            BsonDocument subBson3 = new BsonDocument
+            {
+                {"size", "M"},
+                {"qty", "50"}
+            };
+
+            var documents = new BsonArray { subBson2, subBson3 };
+
+            BsonDocument bson = new BsonDocument
+            {
+                {"item", "ABC1"},
+                {"details", subBson1},
+                {"stock", documents},
+                {"category", "clothing"}
+            };
+
+            BulkWriteOperation bulkWrite = collectionCustomer.InitializeOrderedBulkOperation();
+            bulkWrite.Insert(bson);
+            bulkWrite.Insert(subBson1);
+            bulkWrite.Insert(subBson2);
+            bulkWrite.Insert(subBson3);
+            bulkWrite.Execute();
         }
     }
 }
